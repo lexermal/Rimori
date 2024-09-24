05f15eebf396c3eef533fcd83add46a743c25fdc
lexermal <contact@rimori.se>
2024-09-24 17:34:46 +0200

    chore: renaming of anthropic env variable

--- server/pdf-to-markdown-conversion/src/utils/constants.ts
+++ server/pdf-to-markdown-conversion/src/utils/constants.ts
@@ -1,7 +1,7 @@
 if (!process.env.NEXT_PUBLIC_SUPABASE_URL) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
 if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
 if (!process.env.FRONTEND_DOMAIN) throw new Error('Missing env.FRONTEND_DOMAIN')
-if (!process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY) throw new Error('Missing env.NEXT_PUBLIC_ANTHROPIC_API_KEY')
+if (!process.env.CONVERTING_ANTHROPIC_API_KEY) throw new Error('Missing env.CONVERTING_ANTHROPIC_API_KEY')
 if (!process.env.NEXT_PUBLIC_OPENAI_API_KEY) throw new Error('Missing env.NEXT_PUBLIC_OPENAI_API_KEY')
 if (!process.env.NEXT_PUBLIC_LOGGING_BACKEND_URL) throw new Error('Missing env.NEXT_PUBLIC_LOGGING_BACKEND_URL')
 if (!process.env.NEXT_PUBLIC_LOGGING_USERNAME) throw new Error('Missing env.NEXT_PUBLIC_LOGGING_USERNAME')
@@ -10,7 +10,7 @@ if (!process.env.NEXT_PUBLIC_LOGGING_PASSWORD) throw new Error('Missing env.NEXT
 export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN as string;
-export const ANTHROPIC_API_KEY = process.env.NEXT_PUBLIC_ANTHROPIC_API_KEY as string;
+export const ANTHROPIC_API_KEY = process.env.CONVERTING_ANTHROPIC_API_KEY as string;
 export const OPENAI_API_KEY = process.env.NEXT_PUBLIC_OPENAI_API_KEY as string;
 export const LOGGING_BACKEND_URL = process.env.NEXT_PUBLIC_LOGGING_BACKEND_URL as string;
 export const LOGGING_USERNAME = process.env.NEXT_PUBLIC_LOGGING_USERNAME as string;
